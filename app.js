39958e138a39315d477b2ef8da8c845848b7d190
Mirian <mirianejerenwa@yahoo.com>
2018-10-28 02:27:03 -0400

    Added the pane and contents

--- app.js
+++ app.js
@@ -1,50 +1,43 @@
 const hideAll = function () {
     $('.pane').hide();
-}
-const view=function() { 
+    $('.pane').css('background-color: black');
+    }
+    const view=function() { 
     hideAll();
     let data='';
     for (let i = 0; i < employeeList.length; i++) {
-        data += `<p>${employeeList[i].name}</p> 
-        <p>${employeeList[i].officeNum}</p> 
-        <p>${employeeList[i].phoneNum}</p>`;
+    data += `<div style = 'border: 0.5px solid gray; margin-bottom: 2.5px; width: 400px; line-height: 28px;'>${employeeList[i].name} 
+    <section>${employeeList[i].officeNum}</section> 
+    <section>${employeeList[i].phoneNum}</section> </div>`;
     }
     $('.view').html(data);
     $('.view').render ();
-}
-const showAdd = function () {
+    }
+    
+    const showAdd = function () {
     hideAll();
     $('.add').render ();
-    
-}
+    }
+    const showVerify = function () {
+        hideAll();
+        $('.verify').render ();
+        }
+    const showUpdate = function () {
+        hideAll();
+        $('.update').render ();
+        }
+    const showDelete = function () {
+            hideAll();
+            $('.delete').render ();
+            }
+//     // styles
+// $('.sidebar').css('backgound','blue');
+// $('.content').css('background-color', 'blue');
 
-// $('.addItem').on('click', function () {
-//     const theName = $('.name').val();
-//     const officeNum = $('.officeNumber').val();
-//     const thePhoneNum = $('.phoneNumber').val();
-    
 
-//     const newEmployee = {
-//    theName: theName,
-//    officeNum: officeNum,
-//    thePhoneNum: thePhoneNum
-// };
-// console.log(newEmployee);
-// }
-
-
-
-// Listeners
+// listeners
 $('#view').on('click', view);
 $('#add').on('click', showAdd);
-
-
-
-
-// styles
-// navStyle {
-//     backgound: Black;
-
-// }
-// // const navStyle = $('header');
-// $('header').css('backgound', 'blue');
+$('#verify').on('click', showVerify);
+$('#update').on('click', showUpdate);
+$('#delete').on('click', showDelete);
--- dom.js
+++ dom.js
@@ -70,7 +70,6 @@ const hide = function() {
 }
 const render = function() {
   for (let i = 0; i < nodeList.length; i++) {
-
     nodeList[i].style.display='';}
 }
 
