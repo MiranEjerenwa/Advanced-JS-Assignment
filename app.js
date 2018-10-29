a1eb1dc1cf1c0470d193bae67216bf00a086e681
Mirian <mirianejerenwa@yahoo.com>
2018-10-29 18:56:43 -0400

    functions

--- app.js
+++ app.js
@@ -1,72 +1,114 @@
 // function to dynamically hide all panes
 const hideAll = function () {
     $('.pane').hide();
-    }
-    // function to dynamically allow revealing each pane
-    const view=function() { 
+}
+// function to dynamically allow revealing each pane
+const view = function () {
     hideAll();
-    let data='';
+    let data = '';
     for (let i = 0; i < employeeList.length; i++) {
-    data += `<div style = 'border: 0.5px solid gray; margin-bottom: 2.5px; width: 400px; line-height: 28px;'>${employeeList[i].name} 
+        data += `<div style = 'border: 0.5px solid gray; margin-bottom: 2.5px; width: 400px; line-height: 28px;'>${employeeList[i].name} 
     <section>${employeeList[i].officeNum}</section> 
     <section>${employeeList[i].phoneNum}</section> </div>`;
     }
     $('.view').html(data);
-    $('.view').render ();
-    }
-    
-    const showAdd = function () {
-    hideAll();
-    $('.add').render ();
-    $('.view').render ();
-    }
-    const showVerify = function () {
-        hideAll();
-        $('.verify').render ();
-        }
-    const showUpdate = function () {
-        hideAll();
-        $('.update').render ();
-        }
-    const showDelete = function () {
-            hideAll();
-            $('.delete').render ();
-            }
+    $('.view').render();
+}
 
-    // var itemAdd = $('.content').append('');
+const showAdd = function () {
+    hideAll();
+    $('.add').render();
+    $('.view').render();
+}
+const showVerify = function () {
+    hideAll();
+    $('.verify').render();
+}
+const showUpdate = function () {
+    hideAll();
+    $('.update').render();
+}
+const showDelete = function () {
+    hideAll();
+    $('.delete').render();
+}
+
+// Function to add an employee
 var itemAdd = function () {
+    // Declare object to hold user input
+    const newEmployee = {
+        name: '',
+        officeNumber: '',
+        phoneNumber: ''
+    };
+    // collect user input
     const newName = $('.name').val();
     const newOfficeNumber = $('.officeNumber').val();
     const newPhoneNumber = $('.phoneNumber').val();
-   items = `<div style = 'border: 0.5px solid gray; margin-bottom: 2.5px; width: 400px; line-height: 28px;'>${newName} 
-    <section>${newOfficeNumber}</section> 
-    <section>${newPhoneNumber}</section> </div>`;
 
+    // Add user input to declared object
+    newEmployee.name = newName;
+    newEmployee.officeNumber = newOfficeNumber;
+    newEmployee.phoneNumber = newPhoneNumber;
+    // OR
+    // items = `<div style = 'border: 0.5px solid gray; margin-bottom: 2.5px; width: 400px; line-height: 28px;'>${newName} 
+    //  <section>${newOfficeNumber}</section> 
+    //  <section>${newPhoneNumber}</section> </div>`;
+
+    // Add new employee to the list
     // $('.view').html(items);
     // employeeList.push(items);
     // $('.view').render ();
-    $('.view').append(items);
-    employeeList.push(items);
-    $('.view').render ();
+    // $('.view').append(items);
+    employeeList.push(newEmployee);//saying undefined??
+
+    // clear input 
+    $('.name').val('');
+    $('.officeNumber').val('');
+    $('.phoneNumber').val('');
+   
+    render();
+};
+
+
+
+// function to delete employee
+
+const deleteRec = function () {
+    let existingEmployee = false;
+    // user input
+const deleteName = $('#delName').val();
+// If entered name exists, delete the name
+for (let i = 0; i < employeeList.length; i++) {
+    if (employeeList[i].name === deleteName) {
+        employeeList.splice(i, 1);
+        existingEmployee = true;
+    }
 }
 
+// If employee is not on the list
+if (existingEmployee = false) {
+alert (`'invald employee data'`);
+}
+}
+//function to verify employee
 
-        
+// let verifierImp;
 
-// Functions to add an employee
-
-
-// const itemAdd = function () {
-
-//     const theName = $('.name').val();
-//     const officeNum = $('.officeNumber').val();
-//     const thePhoneNum = $('.phoneNumber').val();
-//     Console.log(val);
-
+// verifierImp ===true
+// if (verifierImp === true) {
+//     $('.verifyNameField').val() = employeeList.indexOf([]);
+//     alert(verifierImp + ' is an employee !')
 // }
-//     // styles
-// $('.sidebar').css('backgound','blue');
-// $('.content').css('background-color', 'blue');
+// else if (verifierImp === false) {
+//     employeeList.indexOf([]);
+//     alert(' Not an employee !')
+//     render ();
+// }
+
+
+
+
 
 
 // listeners to call each function
@@ -76,4 +118,5 @@ $('#verify').on('click', showVerify);
 $('#update').on('click', showUpdate);
 $('#delete').on('click', showDelete);
 $('#addItem').on('click', itemAdd);
-
+// $('#searchItem').on('click', verifierImp);
+$('#searchItem').on('click', deleteRec);
---
+++ app4.js
@@ -0,0 +1,263 @@
+(function () {
+    $(`.inputFields`).hide();
+    $(`#addButton`).hide();
+    $(`#deleteButton`).hide();
+    $(`#searchButton`).hide();
+    $(`#verifyButton`).hide();
+    $(`#updateButton`).hide();
+ 
+ })();
+ 
+ // Hidding all buttons and fields for the View function
+ const showView = function () {
+    $(`.inputFields`).hide();
+    $(`#addButton`).hide();
+    $(`#deleteButton`).hide();
+    $(`#searchButton`).hide();
+    $(`#verifyButton`).hide();
+    $(`#updateButton`).hide();
+    $('#scroll').html('V'.fontcolor('black').bold().big() + 'iew Employee Directory');
+ 
+ };
+ 
+ // Showing the Add function button and fields
+ const showAddInput = function () {
+    $(`.inputFields`).show();
+    $(`#nameInp`).show();
+    $(`#officeNumInp`).show();
+    $(`#phoneNumInp`).show();
+    $(`#addButton`).show();
+    $(`#deleteButton`).hide();
+    $(`#verifyButton`).hide();
+    $(`#updateButton`).hide();
+    $('#scroll').html('');
+    $('#scroll').html('A'.fontcolor('black').bold().big() + 'dd Employees to Directory');
+ 
+ };
+ 
+ // Showing the Delete function button and fields
+ const showDelInput = function () {
+    $(`.inputFields`).show();
+    $(`#nameInp`).show();
+    $(`#officeNumInp`).hide();
+    $(`#phoneNumInp`).hide();
+    $(`#deleteButton`).show();
+    $(`#addButton`).hide();
+    $(`#verifyButton`).hide();
+    $(`#updateButton`).hide();
+    $('#scroll').html('');
+    $('#scroll').html('D'.fontcolor('black').bold().big() + 'elete Employee from Directory');
+ };
+ 
+ // Showing the Verify function button and fields
+ const showVerifyInput = function () {
+    $(`.inputFields`).show();
+    $(`#verifyButton`).show();
+    $(`#deleteButton`).hide();
+    $(`#addButton`).hide();
+    $(`#updateButton`).hide();
+    $(`#officeNumInp`).hide();
+    $(`#phoneNumInp`).hide();
+    $('#scroll').html('V'.fontcolor('black').bold().big() + 'erify Employee in Directory');
+ 
+ };
+ 
+ 
+ // Showing the Update function button and fields
+ const showUpdateInput = function () {
+    $(`.inputFields`).show();
+    $(`#nameInp`).show();
+    $(`#officeNumInp`).show();
+    $(`#phoneNumInp`).show();
+    $(`#updateButton`).show();
+    $(`#deleteButton`).hide();
+    $(`#addButton`).hide();
+    $(`#verifyButton`).hide();
+    $('#scroll').html('');
+    $('#scroll').html('U'.fontcolor('black').bold().big() + 'pdate Employee Information');
+ };
+ 
+ 
+ 
+ // Function for scrolling Welcome text on left nav bar
+ let j = 0;
+ let txt = 'Welcome to the Employee Directory';
+ let speed = 50; /* The speed/duration of the effect in milliseconds */
+ 
+ function typeWriter() {
+    if (j < txt.length) {
+        document.getElementById("scroll").innerHTML += txt.charAt(j);
+        j++;
+        setTimeout(typeWriter, speed);
+    }
+ }
+ typeWriter();
+ 
+ 
+ 
+ 
+ // When the View button is pressed or 'render is called, display all of the employee information in `employeeList` individually in paragraph tags to the div with the class `content`.
+ const render = function () {
+    $('.content').empty();
+ 
+    for (let i = 0; i < employeeList.length; i++) {
+        $('.content').append(`<hr><p>${employeeList[i].name}</p>`);
+        $('.content').append(`<p>${employeeList[i].officeNum}</p>`);
+        $('.content').append(`<p>${employeeList[i].phoneNum}</p>`);
+    }
+ }
+ 
+ render();
+ 
+ // Listening for user to click the View menu option and then render the employee list.
+ $('#view').on('click', showView);
+ $('#view').on('click', render);
+ 
+ //  When the add button is pressed, use the `val` function to get the value of the user input and and add that name to the list. Re-render the list.
+ const getInputVal = function () {
+    // Delaring newEmployee object elements that will be used to gather user imput and then add to the Employee list.
+    const newEmployee = {
+        name: '',
+        officeNum: '',
+        phoneNum: ''
+    };
+ 
+    // Gathering user input.
+    const nameVal = $('#nameInp').val();
+    const officeVal = $('#officeNumInp').val();
+    const phoneVal = $('#phoneNumInp').val();
+ 
+    // Assigning user input to newEmployee object.
+    newEmployee.name = nameVal;
+    newEmployee.officeNum = officeVal;
+    newEmployee.phoneNum = phoneVal;
+ 
+    // Adding the new employee infomation to the employee list.
+    employeeList.push(newEmployee);
+ 
+    // Alert user of action status.
+    $('#scroll').html('Employee added to Directory');
+ 
+    // After performing our actions, clear the input values and re-render the list
+    $('#nameInp').val('');
+    $('#officeNumInp').val('');
+    $('#phoneNumInp').val('');
+    render();
+ 
+ }
+ // Listening for user to click the Add menu option.
+ $(`#add`).on(`click`, showAddInput);
+ // Listening for the user to push the Add buttom.
+ $('#addButton').on('click', getInputVal);
+ 
+ 
+ 
+ //  When the delete button is pressed, delete the element from employeeList that matches the name the user entered in the input field. Re-render the list.
+ const removeName = function () {
+    let onEmployeetList = false;
+    // Gathering user input.
+    const nameVal = $('#nameInp').val();
+ 
+    // If selected employee name is in the list, delete the employee record.
+    for (let i = 0; i < employeeList.length; i++) {
+        if (employeeList[i].name === nameVal) {
+            employeeList.splice(i, 1);
+            onEmployeetList = true;
+        }
+    }
+ 
+    // Alert user of action status.
+    $('#scroll').html(nameVal.fontcolor("DarkBlue").bold() + ' removed from Directory');
+ 
+    // If selected employee name not in the list, alert user.
+    if (onEmployeetList == false) {
+        $('#scroll').html(nameVal.fontcolor("DarkRed").bold() + ' not in the employee list');
+    }
+ 
+    // After performing our actions, clear the input values and re-render the list
+    $('#nameInp').val('');
+    $('#officeNumInp').val('');
+    $('#phoneNumInp').val('');
+    render();
+ 
+ }
+ 
+ // Listening for user to click the Delete menu option.
+ $(`#delete`).on(`click`, showDelInput);
+ // Listening for the user to push the Delete buttom.
+ $('#deleteButton').on('click', removeName);
+ //  When the Verify button is pressed,  check to see if the name the user entered is in the employee List.
+ const verifyName = function () {
+    // Gathering user input and initializing variable 'listStatus'.
+    const nameVal = $('#nameInp').val();
+    let listStatus = false;
+ 
+    // Loop through employeeList and set the listStatus to 'true' or 'false' based on userinput value.
+    for (let i = 0; i < employeeList.length; i++) {
+        if (employeeList[i].name === nameVal) {
+            listStatus = true;
+            break
+        } else {
+            listStatus = false;
+        }
+    }
+ 
+    // Based on booloean value of listStatus, use html method to alert user of action status.
+    if (listStatus == true) {
+        $('#scroll').html(nameVal.fontcolor("DarkBlue").bold() + ' is in the employee list');
+ 
+    } else {
+        $('#scroll').html(nameVal.fontcolor("DarkRed").bold() + ' not in the employee list');
+ 
+    }
+ 
+    // After performing our actions, clear the input values and re-render the list
+    $('#nameInp').val('');
+ 
+ }
+ // Listening for user to click the Verify menu option.
+ $(`#verify`).on(`click`, showVerifyInput);
+ $('#verifyButton').on('click', verifyName);
+ 
+ 
+ 
+ 
+ //  When the Update button is pressed, allows the user to input name, office number, and phone number and then update the office number and phone number
+ //  of the employee that matches the input name, and then renders the updated employee list.
+ const updateName = function () {
+    let onEmployeetList = false;
+ 
+    // Gathering user input.
+    const nameVal = $('#nameInp').val();
+    const officeVal = $('#officeNumInp').val();
+    const phoneVal = $('#phoneNumInp').val();
+ 
+    // If selected empoyee is on the list, update with new employee infomation.
+    for (let i = 0; i < employeeList.length; i++) {
+        if (employeeList[i].name === nameVal) {
+            employeeList[i].name = nameVal;
+            employeeList[i].officeNum = officeVal;
+            employeeList[i].phoneNum = phoneVal;
+            onEmployeetList = true;
+        }
+    }
+ 
+    // Alert user of action staus.
+    $('#scroll').html('Directory has been updated');
+ 
+    // If selected employee name not in the list, alert user.
+    if (onEmployeetList == false) {
+        $('#scroll').html(nameVal.fontcolor("DarkRed").bold() + ' is not in the Directory');
+    }
+ 
+ 
+    // After performing our actions, clear the input values and re-render the list
+    $('#nameInp').val('');
+    $('#officeNumInp').val('');
+    $('#phoneNumInp').val('');
+    render();
+ 
+ }
+ // Listening for user to click the Update menu option.
+ $(`#update`).on(`click`, showUpdateInput);
+ $('#updateButton').on('click', updateName);
